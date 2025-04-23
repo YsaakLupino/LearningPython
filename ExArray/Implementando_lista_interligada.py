d4a5a1b924106400c5d5601fd25216ca5346c4fe
YsaakLupino <ysaaklupino327@gmail.com>
2025-04-23 11:19:01 -0300

    Tratamento e comentários
    Tratei erros que poderiam ocorrer se a lista tivesse vazia ou se os parâmetros inseridos para as funções não atenderem a lista existente. Além disso comentei melhor o código

--- ExArray/Implementando_lista_interligada.py
+++ ExArray/Implementando_lista_interligada.py
@@ -1,78 +1,88 @@
 class organizada:
     def __init__(self):
-        self.inicio = None
+        self.inicio = None #Head
     
     class no:
         def __init__(self,atual):
             self.atual = atual
             self.proximo = None
     
-    def addend(self, atual): #adicionar no final
-        dado = organizada.no(atual)  
+    #Funções:
+    def addend(self, atual): # Adicionar nó ao final da lista
+        dado = organizada.no(atual) 
         if self.inicio != None:
             temp = self.inicio
             while temp.proximo != None:
                 temp = temp.proximo
             temp.proximo = dado
-            return dado
+            return
             
         self.inicio = dado
-        return dado
+
     
-    def exibir(self): #exibir sua lista
+    def exibir(self): # Exibir sua lista
         temp = self.inicio
         if self.inicio == None:
-            raise IndexError('Lista vazia')
-        print('[', end='')
+            raise IndexError('Lista vazia') 
+        print('[', end='') # Parte visual para parecer uma lista de python
         while temp.proximo != None:
             print(f'{temp.atual}', end=', ')
             temp = temp.proximo
         print(f'{temp.atual}]')
     
-    def addini(self, atual): #adicionar no inicio
+    def addini(self, atual): #adicionar nó no inicio
         dado = organizada.no(atual)
         temp = self.inicio
         self.inicio = dado
-        self.inicio.proximo = temp
+        self.inicio.proximo = temp # não necessita de if lista vazia
         
-    def removini(self):
+    def removini(self): #Remover primeiro nó
+        if self.inicio == None:
+            raise IndexError('Lista Vazia') # controle erro contra lista vazia 
         temp = self.inicio.proximo
         self.inicio = temp
     
-    def removend(self):
+    def removend(self): #Remover ultimo nó
+        if self.inicio == None:
+            raise IndexError('Lista Vazia') # controle erro contra lista vazia 
         temp = self.inicio
         while temp.proximo.proximo != None:
             temp = temp.proximo
         print(temp.atual)
         temp.proximo = None
 
-    def removeval(self, n):
+    def removeval(self, n): # Remover nó por seu valor atual
+        if self.inicio == None:
+            raise IndexError('Lista Vazia') # controle erro contra lista vazia 
         temp = self.inicio
         while True:
             if temp.atual == n:
                 self.inicio = temp.proximo
                 break
-            temp2 = temp
+            temp2 = temp # Guardando temp anterior ao do valor a ser removido para atualizar seu proximo depois
             temp = temp.proximo
             while True:
-                if temp.atual == n:
+                try: # Tratando erro caso nao exista o valor na lista
+                    if temp.atual == n:
+                        temp = temp.proximo
+                        temp2.proximo = temp
+                        break
+                    temp2 = temp
                     temp = temp.proximo
-                    temp2.proximo = temp
-                    break
-                temp2 = temp
-                temp = temp.proximo
+                except AttributeError:
+                    break # Simplesmente nao acontece nada caso o elemento nao exista na lista
             break
     
     def removepstn(self, p):
         temp = self.inicio
-        while True:
-            cont = 1
-            if p == cont:
-                self.inicio = temp.proximo
-                break
-            temp2 = temp
-            temp = temp.proximo
-            cont += 1
+        cont = 0
+        if p == cont: #caso o primeiro no ja o procurado
+            self.inicio = temp.proximo 
+            return
+        temp2 = temp
+        temp = temp.proximo
+        cont += 1
+        try: # Tratando erro caso posição nao exista! 
             while True:
                 if p == cont:
                     temp = temp.proximo
@@ -81,9 +91,19 @@ class organizada:
                 temp2 = temp
                 temp = temp.proximo
                 cont += 1
-                break
-            break
-             
+        except AttributeError:
+            return # Nada acontece caso a posição não exista!
+
+
+
+#Testes a se fazer:
+
+'''
+Adicionar função print atraves de dunder methods
+'''
+'''
+Adicionar parametro de posição 1 2 3.. ao removini e removendo
+'''  
 
 lista = organizada()
 lista.addend(1)
@@ -91,7 +111,6 @@ lista.addend(2)
 lista.addend(3)
 lista.addend(4)
 lista.exibir()
-
 lista.removeval(4)
 lista.exibir()
 lista.addend(4)
@@ -113,4 +132,13 @@ lista.addini(4)
 lista.addini(3)
 lista.addini(2)
 lista.addini(1)
+lista.removepstn(12)
+lista.removepstn(1)
 lista.exibir()
+
+lista2 = organizada()
+
+lista2.addini(12)
+lista2.addini(121)
+lista2.removeval(121)
+lista2.exibir()

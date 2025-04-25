a69ac01bb836fd1b4078753a98e2b2fdfdf3b1ff
YsaakLupino <ysaaklupino327@gmail.com>
2025-04-25 17:30:56 -0300

    criação arquivo analise-filmes

---
+++ ExAPI/analise-filmes.py
@@ -0,0 +1,10 @@
+'''
+🔸 Criar um script que:
+Pega vários filmes da API - filtro ano
+Salva os dados em .csv
+Lê esse .csv com pandas
+Cria gráficos com matplotlib ou seaborn (ex: quantidade de filmes por idioma, nota média por diretor etc.)
+'''
+import requests
+
+f'http://www.omdbapi.com/?y=2020&apikey=b20caf44'

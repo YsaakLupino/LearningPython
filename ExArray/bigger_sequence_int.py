ef89f66234d3ae98828d15c9b778d485a7d996e2
YsaakLupino <ysaaklupino327@gmail.com>
2025-05-06 22:42:41 -0300

    Docstrings
    Colquei docstrings no documento da bigger sequence, késimo.. etc

--- ExArray/bigger_sequence_int.py
+++ ExArray/bigger_sequence_int.py
@@ -1,40 +1,64 @@
-'''
-Ao receber o input de uma lista de int, o programa deve identificar qual a maior
+"""Exercícios video - agusto galego.
+
+3 problemas que mais caem em entrevistas do vídeo https://www.youtube.com/watch?v=sjqB70KIfog
+
+    1) Ao receber o input de uma lista de int, o programa deve identificar qual a maior
 squencia formada dentro do array e em seguida contar quantos elementos tem 
 (o comprimento dessa sequencia). Ele retorna este dado.
 
 Exemplo:
-    Input = [1,4,6,23,5,2,9,11]
-    Output = 3 pois a maior sequencia é [4,5,6] e o tamanho é de 3 elementos
-'''
+Input = [1,4,6,23,5,2,9,11]
+Output = 3 pois a maior sequencia é [4,5,6] e o tamanho é de 3 elementos
 
-nums = [1,0,4,3,23,45,46,47,48,49,50,51,52,5,2,9,11]
-nums_dict = {}
-sequence_lenght = 1
-for num in nums:
-    nums_dict[num] = num
-print(nums_dict)
+    2) Dado um array de ints 'nums' e um inteiro 'K' retorne o(s) k elemento(s) mais
+frequente(s) na lista
 
-for num in nums:
-    if num-1 not in nums_dict:
-        sequence_lenght_temp = 1
-        while num+1 in nums_dict:
-            sequence_lenght_temp += 1
-            num+=1
-        if sequence_lenght_temp >= sequence_lenght:
-            sequence_lenght = sequence_lenght_temp
+Exemplo:
+Input = [1,1,1,2,2,3,3]
+K = []
+"""
+class Exercicios():
+    """Representa exercicios do vídeo."""
 
-print(sequence_lenght)
+    def __init__(self):
+        """Setter classe Exercícios."""
+        pass
 
-'''
+    def exercício_1(self, nums_list:list[int]) -> int:
+        """Realiza execução do exercício 1.
+
+        Printa o resultado do exercicio
+
+        Args:
+            nums_list (list[int]): Lista a analisar
+        
+        Returns:   
+            tudo
+            none      
+        """
+        nums = nums_list
+        nums_dict = {}
+        sequence_lenght = 1
+        for num in nums:
+            nums_dict[num] = num
+
+        for num in nums_dict:
+            if num-1 not in nums_dict:
+                sequence_lenght_temp = 1
+                while num+1 in nums_dict:
+                    sequence_lenght_temp += 1
+                    num+=1
+                if sequence_lenght_temp >= sequence_lenght:
+                    sequence_lenght = sequence_lenght_temp
+        print(f'A maior sequencia da lista tem o tamanho de {sequence_lenght} números')
+
+
+"""
 eu compreendi que o melhor jeito nao era percorrer ele varias vezes mas nao
 consegui fazer sozinho, eu peguei a ideia da solução no video mas a codagem eu desenvolvi
-'''
+"""
 
-'''
+"""
 Como desafio ficam os outros 2 problemas
  do vídeo https://www.youtube.com/watch?v=sjqB70KIfog
-'''
-
-
-    
+"""

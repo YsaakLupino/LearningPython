de66a19ab7bc6f4d0b800ef90b4577dafd49ca97
YsaakLupino <ysaaklupino327@gmail.com>
2025-05-14 00:57:50 -0300

    OOP in rename file

--- ExOSPytAutoGui/rename_arq.py
+++ ExOSPytAutoGui/rename_arq.py
@@ -5,15 +5,23 @@ import os
 #renomear pastas (nome/path antigo, nome/path novo)
 # os.rename('teste',
 #            'mod')
-PATH_TO_WORK = r'D:\pinoso\programação\LearningPython\ExGerais\ExerciciosForPrint' #pasta a explorar
-DIR_LIST = os.listdir(PATH_TO_WORK)
+
+class OSchanger():
+    def __init__(self):
+        pass
+    def change_file_nem(self, path_to_change:str, logic_name:str ):
+        PATH_TO_WORK = path_to_change #pasta a explorar
+        DIR_LIST = os.listdir(PATH_TO_WORK)
+        for n, y in enumerate(DIR_LIST,1):
+            if y.find('.') > 0: #quais arquivos modificar
+                new_name_logic = f'{logic_name}_0{n}.py' #nome do arquivo 
+                path_to_change = rf'{PATH_TO_WORK}\{y}'
+                print(path_to_change)
+                path_new_name = rf'{PATH_TO_WORK}\{new_name_logic}'
+                os.rename(path_to_change,path_new_name)
+                
+        return None
 
 
 
-for n, y in enumerate(DIR_LIST,1):
-    if y.find('.') > 0: #quais arquivos modificar
-        new_name_logic = f'Ex_for0{n}.py' #nome do arquivo 
-        path_to_change = rf'{PATH_TO_WORK}\{y}'
-        print(path_to_change)
-        path_new_name = rf'{PATH_TO_WORK}\{new_name_logic}'
-        os.rename(path_to_change,path_new_name)
+

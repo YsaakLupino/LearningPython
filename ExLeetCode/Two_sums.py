f124f4a8f4ec5914dfb36ed7333791224664f42c
YsaakLupino <ysaaklupino327@gmail.com>
2025-05-14 01:28:41 -0300

    Update Two_sums.py
    solução ruim mas tentei

--- ExLeetCode/Two_sums.py
+++ ExLeetCode/Two_sums.py
@@ -1,1 +1,18 @@
-array = [11,15,2,7 ]
+array = [11,15,2,7]
+target = 9
+
+
+for idx, p1 in enumerate(array,0):
+    if p1 > 9:
+        continue
+    lf_num = 9 - p1
+    for idx2, p2 in enumerate(array[idx+1:], idx+1):
+        if p2 == lf_num:
+            resultado = (idx, idx2 )
+        
+print(resultado)
+            
+
+
+
+# solução em O(2n²+1)

e4718cfd91ddac66482b5551524bf695327fe34c
YsaakLupino <ysaaklupino327@gmail.com>
2025-05-14 00:39:30 -0300

    snake case em .py da exgerais
    usei o arquivo de renomear para botar padrao snake case nos .py da pasta exgerais

--- ExOSPytAutoGui/rename_arq.py
+++ ExOSPytAutoGui/rename_arq.py
@@ -5,15 +5,14 @@ import os
 #renomear pastas (nome/path antigo, nome/path novo)
 # os.rename('teste',
 #            'mod')
-PATH_TO_WORK = r'D:\pinoso\programação\LearningPython\ExGuanabara'
+PATH_TO_WORK = r'D:\pinoso\programação\LearningPython\ExGerais\ExerciciosForPrint' #pasta a explorar
 DIR_LIST = os.listdir(PATH_TO_WORK)
 
 
 
 for n, y in enumerate(DIR_LIST,1):
-    print(y)
-    if y[0] == 'e':
-        new_name_logic = f'Ex_py0{n}.py'
+    if y.find('.') > 0: #quais arquivos modificar
+        new_name_logic = f'Ex_for0{n}.py' #nome do arquivo 
         path_to_change = rf'{PATH_TO_WORK}\{y}'
         print(path_to_change)
         path_new_name = rf'{PATH_TO_WORK}\{new_name_logic}'

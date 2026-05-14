9032542cf3d92ae54f0c8e48ed22c23b292a5f78
YsaakLupino <ysaaklupino327@gmail.com>
2026-05-14 20:21:50 -0300

    Mudei vetor tupla

--- generatorobject.py
+++ generatorobject.py
@@ -1,4 +1,4 @@
-vetortupla = (input('Insira um numero:') for n in range(5))
+vetortupla = (input(f'Insira o {n+1}º numero:') for n in range(5))
 print(vetortupla)
 tupla = tuple(vetortupla)
 print(tupla)

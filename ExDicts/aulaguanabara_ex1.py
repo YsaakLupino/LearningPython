91b0b81ce62c19fdfa7fa628667bdba1a3b99530
suporte-ti2 <ysaaklupino327@gmail.com>
2026-07-21 07:48:24 -0300

    Create aulaguanabara_ex1.py

---
+++ ExDicts/aulaguanabara_ex1.py
@@ -0,0 +1,21 @@
+'''
+ler nome e media de um aluno e mostre na tela
+
+
+'''
+
+info_aluno = {}
+
+for i in range(0,3):
+
+    nome = str(input('Digite o nome do aluno: '))
+
+    while True:
+        try:
+            media = float(input('Digite o nome do aluno: '))
+            break
+        except ValueError:
+            print('digite um float')
+        
+    {}
+    

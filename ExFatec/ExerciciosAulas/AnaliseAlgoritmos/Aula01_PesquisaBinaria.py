4564590e8498640798c930d470a1d9856b18213b
suporte-ti2 <ysaaklupino327@gmail.com>
2026-08-20 15:06:29 -0300

    código incompleto
    preciso definir num papel o que eu quero fazer antes, estou perdendo tempo pensando!

---
+++ ExFatec/ExerciciosAulas/AnaliseAlgoritmos/Aula01_PesquisaBinaria.py
@@ -0,0 +1,47 @@
+'''
+Documento designado a estudar diferença entre busca linear e pesquisa binaria
+e comparando uso tempo e análise Big O. 
+'''
+import numpy as np
+
+class Vetor():
+    def __init__(self,vetor):
+        '''
+        classe para criar vetor garantindo que todos os objetos contidos nele tenha o mesmo tipo
+        '''
+        self.vetor = vetor
+        self.tipo = None
+
+
+    def _define_type(self, vetor) -> None:
+        '''
+        Define o tipo do array
+        '''
+        
+
+
+
+class Search():
+    '''
+    Classe que representa o que toda busca tem que ter
+    '''
+    def __init__(self):
+        pass
+
+    def _type_of_search(self,type):
+        '''
+        define o tipo de busca a fazer
+        '''
+        if type = 'binary':
+            
+            
+
+    def _isOrdered(self, vetor) -> bool:
+        pass
+
+    class BinarySearch():
+        def __init__(self):
+            pass
+            
+
+

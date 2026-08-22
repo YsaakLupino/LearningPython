baa12b53a3a7512c862a13cf0250f7813609cfcc
YsaakLupino <ysaaklupino327@gmail.com>
2026-08-22 14:12:40 -0300

    fazendo o exercicio

--- ExFatec/ExerciciosAulas/AnaliseAlgoritmos/Aula01_PesquisaBinaria.py
+++ ExFatec/ExerciciosAulas/AnaliseAlgoritmos/Aula01_PesquisaBinaria.py
@@ -1,11 +1,26 @@
 from time import time
 
-listt = list(range(1,1_000_001)) #1 to 1 million test list 
-print(listt)
+listt = list(range(1,501)) #1 to 1 million test list 
+
 
 def binary_search(vetor:list[int],value:int) -> bool|int:
     '''
     manual implementation of a binary search for studies
     '''
-    minor = 0
-    upper = 
+    
+    down = 0
+    upper = len(vetor)-1
+    middle = round (upper-down/2)
+    vetor = sorted(vetor)
+
+    while True:
+        if value == vetor[middle]:
+            return middle
+        elif value > vetor[middle]:
+            down = middle + 1
+        elif value < vetor[middle]:
+            upper = middle -1
+
+        middle = round (upper-down/2)
+
+print(binary_search(listt,120))
